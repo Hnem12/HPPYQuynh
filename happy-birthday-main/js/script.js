0de911d553184f8ac5e868548f5109300ddab872
Hnem <149763377+Hnem12@users.noreply.github.com>
2025-03-08 21:32:52 +0700

    Update script1.js

--- happy-birthday-main/js/script.js
+++ happy-birthday-main/js/script.js
@@ -1,26 +1,26 @@
-function checkValid() {
-    var input = document.getElementById("nhapngaysinh").value;
-    
-    if (input === "08/03/2003") {
-        window.location.href = "/Nextpage.html"; // Chuyển đến trang tiếp theo
-    } else {
-        alert("Ngày sinh không chính xác!");
+    function checkValid() {
+        var input = document.getElementById("nhapngaysinh").value;
+        
+        if (input === "08/03/2003") {
+            window.location.href = "happy-birthday-main/Nextpage.html"; // Chuyển đến trang tiếp theo
+        } else {
+            alert("Ngày sinh không chính xác!");
+        }
     }
-}
 
-document.getElementById("nhapngaysinh").addEventListener("keyup", function(e) {
-    if (e.key === "Enter" || e.keyCode === 13) {
-        checkValid();
+    document.getElementById("nhapngaysinh").addEventListener("keyup", function(e) {
+        if (e.key === "Enter" || e.keyCode === 13) {
+            checkValid();
+        }
+    });
+
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
-});
 
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-$("#nhapngaysinh").on('keyup', function(e) {
-    if (e.key === 'Enter' || e.keyCode === 13) {
-        checkValid();
-    }
-});
+    $("#nhapngaysinh").on('keyup', function(e) {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            checkValid();
+        }
+    });
